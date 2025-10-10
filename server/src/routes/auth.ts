1502ad440d2a779c66aaf3488f3784f0fd765cd9
Lais Calderaro <laiscalderaro06@gmail.com>
2025-10-10 09:59:23 -0400

    [FEAT] Add router login

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import { pool } from '../db';
 import bcrypt from 'bcrypt';
+import jwt  from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -44,6 +45,49 @@ router.post('/register', async (req, res) => {
         res.status(500).json({ error: 'Erro ao cadastrar usuário' });
     }
 });
+
+// Login de usuário
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    console.log("Tentando login com:", email);
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email e senha são obrigatórios' });
+    }
+
+    try {
+        const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+        const user = result.rows[0];
+
+        if (!user) {
+            return res.status(400).json({ error: 'Usuário não encontrado' });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.senha);
+        if (!validPassword) {
+            return res.status(400).json({ error: 'Senha incorreta' });
+        }
+
+        // Gera token JWT
+        const jwt = await import('jsonwebtoken');
+        const token = jwt.default.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET || 'chave_secreta',
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({
+            message: 'Login realizado com sucesso',
+            token,
+            user: { id: user.id, name: user.nome, email: user.email },
+        });
+    } catch (error) {
+        console.error('Erro no login:', error);
+        res.status(500).json({ error: 'Erro ao fazer login' });
+    }
+});
+
 
 // Adicione no final do arquivo src/routes/auth.ts
 router.get('/users', async (req, res) => {
