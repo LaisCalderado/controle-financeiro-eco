8ba80f59926e406b57de995baffe851a31bb1dcc
Lais Calderaro <laiscalderaro06@gmail.com>
2025-10-10 10:02:32 -0400

    [FEAT] Not found page

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return (
+        <div className="login-page">
+            <div className="login-container">
+                <h2>Página não encontrada</h2>
+                <p>O caminho acessado não existe 😢</p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
